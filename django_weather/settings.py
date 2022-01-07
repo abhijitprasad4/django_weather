6c943f1a58bbc7a0ee02196b7cfae689ca24de15
abhijitprasad4 <94544539+abhijitprasad4@users.noreply.github.com>
2022-01-07 22:42:33 +0530

    Update settings.py

--- django_weather/settings.py
+++ django_weather/settings.py
@@ -123,14 +123,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
         os.path.join(BASE_DIR, 'boot'),
-)
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
