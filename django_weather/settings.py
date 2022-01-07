b9d0aff5b7783d4ca30e68fde789dcee7973bc79
abhijitprasad4 <94544539+abhijitprasad4@users.noreply.github.com>
2022-01-07 22:46:13 +0530

    Update settings.py

--- django_weather/settings.py
+++ django_weather/settings.py
@@ -123,11 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-        os.path.join(BASE_DIR, 'boot'),
-]
-# Default primary key field type
+# ault primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
