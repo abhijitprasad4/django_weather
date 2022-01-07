eb927e12683885af06a12b838bb5bbf06a6f3413
abhijitprasad4 <94544539+abhijitprasad4@users.noreply.github.com>
2022-01-07 22:32:20 +0530

    Update settings.py

--- django_weather/settings.py
+++ django_weather/settings.py
@@ -123,11 +123,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS =[
+STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
         os.path.join(BASE_DIR, 'boot'),
-
-]
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
